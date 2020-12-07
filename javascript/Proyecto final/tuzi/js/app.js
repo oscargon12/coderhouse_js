06849f7ed8ed5ebba9c6e2b8c66b00628b8c7aa4
oscargon12 <oscargon12@gmail.com>
2020-12-07 17:49:31 -0500

    Subiendo entrega 1

--- javascript/Proyecto final/tuzi/js/app.js
+++ javascript/Proyecto final/tuzi/js/app.js
@@ -1,5 +1,5 @@
 
-// El proyecto tiene dos fases:
+// Proyecto: SPA de dos fases
 // Entrada: El usuario registra alimentos y su fecha de vencimiento
 // Salida: La app alerta alimentos a vencer y genera recetas con estos productos
 
@@ -11,7 +11,9 @@
 /* function pedirIngrediente() {
   let ingrediente = prompt('Ingresa el nuevo ingrediente');
   let fecha = prompt('Ingresa la fecha de vencimiento');
-  let hora = prompt('Elije una hora del día');
+  let hora = prompt('Del 1 al 4 elije una hora del día');
+
+// Esto me puede servir para la generación de recetas
 
   if (hora == 1) {
     tipoComida = 'desayuno';
@@ -38,7 +40,9 @@ pedirIngrediente()
 // Incorporarle sus propiedades y su constructor
 // Invocar a ese objeto en algún momento donde el usuario realice alguna acción
 
-function Ingrediente(categoria, nombre, vencimiento,) {
+// creo uno fijo para pruebas y uno que pide datos al usuario
+
+function IngredienteUsuario(categoria, nombre, vencimiento,) {
   this.categoria = categoria;
   this.nombre = nombre;
   this.vencimiento = vencimiento;
@@ -48,14 +52,37 @@ function Ingrediente(categoria, nombre, vencimiento,) {
   };
 }
 
-/* //EL proyecto requiere pedir datos de entrada al usuario
-let categoria = prompt('Ingresa la categoria del producto');
-let nombre = prompt('Ingresa el nombre del producto');
+//EL proyecto requiere pedir datos de entrada al usuario
+let categoria = prompt('Ingresa la categoria del alimento a registrar (Ejem: lacteos, enlatados)');
+let nombre = prompt('Ingresa el nombre del alimento a registrar');
 let vencimiento = prompt('Ingresa la fecha de vencimiento');
 
 //Se instancia el objeto
-let ingrediente1 = new Ingrediente(categoria, nombre, vencimiento);
-console.log(ingrediente1) */
+let ingrediente1 = new IngredienteUsuario(categoria, nombre, vencimiento);
+console.log(ingrediente1)
+
+function Ingrediente(categoria, nombre, vencimiento,) {
+  this.categoria = categoria;
+  this.nombre = nombre;
+  this.vencimiento = vencimiento;
+
+  this.mostrarInfo = function () {
+    console.log(`El producto ${this.nombre} vence el ${this.vencimiento}`);
+  };
+}
+// Instanciando...
+let ingrediente2 = new Ingrediente('Lacteos', 'Leche', '08-12-2020');
+console.log (ingrediente2);
+
+// Arreglo con mis productos (lo que llamo al objeto es la instancia)
+const productos = [ingrediente1, ingrediente2];
+
+// Agrego un nuevo ingrediente
+productos.push({categoria: "Cereales",
+nombre: 'arroz',
+vencimiento: '15-01-2020',});
+
+console.table(productos);
 
 
 // Constructora 2
@@ -70,9 +97,9 @@ function Receta(nombre, ingredientes, preparacion,) {
 }
 
 let receta2 = {
-  nombreReceta: 'lasagna',
-  ingre: ['pasta', 'bolognesa'],
-  prepara: 'horno',
+  nombreReceta: 'Arroz de leche',
+  ingre: ['Arroz', 'leche', 'Leche condensada', 'Uvas pasas', 'canela'],
+  prepara: 'estufa',
 }
 
 console.log(receta2);
